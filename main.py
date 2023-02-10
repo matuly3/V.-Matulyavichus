c1c0635b2f54b6e16d557cfc7733343c6a8c0630
matuly3 <124338594+matuly3@users.noreply.github.com>
2023-02-10 11:40:23 +0300

    Классная практическая работа

---
+++ main.py
@@ -0,0 +1,46 @@
+text = "Hello, Python!"
+print (text[0])
+print (text[0:5])
+print (text[4:10])
+print (text[0:14])
+print (text[7:])
+print (text[:5])
+print (text[:])
+print (text[-1])
+print (text[-1:-14])
+print (text[::2])
+print (text[::-1])
+print (text + "ok") 
+print (text[-1])
+
+
+
+
+my_list =[True, 786, 3,14, 'text', 70,2]
+second_list = [123, 'text']
+
+print (my_list) 
+print (my_list [0])
+print (my_list [1:3])
+print (my_list [2:])
+print (second_list *2)
+print (my_list  + second_list)
+
+
+
+dad =(True, 786, 3.14, 'text', 70.2)
+bab =(123, 'text')
+
+print (dad)
+print (dad[0])
+print (bab *2)
+print (dad + bab)
+
+
+
+my_dict = {}
+my_dict["countri"] = "mexico"
+
+another_dict = {"number":23, 2: True, "my_list":[1,2,3]}
+print (another_dict.keys())
+print (another_dict.values())
