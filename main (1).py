d6042c1b3585e48199f27c3a6d3135d114e0a976
matuly3 <124338594+matuly3@users.noreply.github.com>
2023-02-10 11:47:03 +0300

    Практическая классная работа

---
+++ main (1).py
@@ -0,0 +1,42 @@
+text = "Hello, Python"
+print (text[0])
+print (text[0:5])
+print (text[4:10])
+print (text[0:14])
+print (text[7:])
+print (text[:5])
+print (text[:])
+print (text[-1])
+print (text[-1:-14])
+print (text[::2])
+print (text[::-1])
+print (text + "I love chips")
+print (text[-1] * 10)
+my_list = [True, 786, 3.14, "text", 70.2]
+second_list = [123, "text"]
+print (my_list)
+print (my_list[0])
+print (my_list[1:3])
+print (my_list[2:])
+print (second_list *2)
+print (my_list + second_list)
+my_tuple = (True, 786, 3.14, 'text', 70.2)
+second_tuple = (123, "text")
+print (my_tuple)
+print (my_tuple[0])
+print (second_tuple *2)
+print (my_tuple + second_tuple)
+my_dict = { }
+my_dict["country"] = "Mexico"
+print (my_dict["country"])
+another_dict = {"number":23, 2: True, "my_list" :[1,2,3]}
+print (another_dict.keys())
+print (another_dict.values())
+s = set()
+s = {"hi", "bye"}
+print (s)
+a = set()
+a.add(1)
+a.add(2)
+print(a)
+
